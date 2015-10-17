c293ccd593a42af98a741ecd08fb6f9753a60558
David Ochmann <github@davidochmann.de>
2015-10-17 18:07:43 +0200

    - Added scale handling.

--- Assets/Scripts/Component/ScaleText.cs
+++ Assets/Scripts/Component/ScaleText.cs
@@ -2,8 +2,14 @@ using UnityEngine;
     	
 public class ScaleText : MonoBehaviour
 {
+	public GameObject scaleGameObject;
+
+	new private Collider2D collider2D;
+
 	private MeshRenderer meshRenderer;
-	private Vector3 size;
+	private Vector3 meshSize;
+	private Vector3 colliderSize;
+	private Vector3 scale;
 
 
 	/**
@@ -22,9 +28,10 @@ public class ScaleText : MonoBehaviour
 
 	public void init()
 	{
-		if( size == default( Vector3 )  )
+		if( meshSize == default( Vector3 )  )
 			initVariables();
 		else
+		if( scale == default( Vector3 ) )
 		{
 			initScaling();
 		}
@@ -35,13 +42,33 @@ public class ScaleText : MonoBehaviour
 	private void initVariables()
 	{
 		meshRenderer = GetComponent<MeshRenderer>();
-		size = meshRenderer.bounds.center;
+		collider2D = scaleGameObject.GetComponent<Collider2D>();
+		meshSize = meshRenderer.bounds.size;
+		colliderSize = collider2D.bounds.size;
 	}
 
 
 	/** Scaling functions. */
 	private void initScaling()
 	{
+		if( collider2D )
+		{
+			float p = 1;
+			float m = .8f;
 
+			if( meshSize.x > meshSize.y )
+				p = meshSize.x / colliderSize.x;
+			else
+				p = meshSize.y / colliderSize.y;
+
+			scale = new Vector3();
+
+			scale.x = ( transform.localScale.x / p ) * m;
+			scale.y = ( transform.localScale.y / p ) * m;
+
+			transform.localScale = scale;
+		}
+		else
+			Debug.LogWarning( "scaleGameObject has to have component of type Collider2D.");
 	}
 }
